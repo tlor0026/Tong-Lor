c0248c3a9b0e10f8148048cb84d1245cc3fc5d53
tlor0026 <tlor0026@gmail.com>
2022-10-14 16:53:10 -0500

    resized images

--- src/components/about.js
+++ src/components/about.js
@@ -13,12 +13,12 @@ function About() {
     return(
         <div class="jumbotron">
   <h1 class="display-4">About Me</h1>
-  <img class="headshot" src= {photo} width="450px" height="600px" alt="headshot" style={imgStyle}></img>
+  <img class="headshot" src= {photo} width="480px" height="800px" alt="headshot" style={imgStyle}></img>
   <p class="lead">Welcome to my portfolio. I've been in the IT field for about 7 years now and decided to transition into software development/programing. Life is all about learning and being dedicated to building yourself up to new and better things. I've just completed my Coding boot camp with the University of Minnesota that went through a full stack course. This includes front and back end components, the user of server and 3rd party API's, React, Javascript, databases along with many other things. Im looking forward in life to more things to come!</p>
   <hr class="my-4"></hr>
   <p></p>
   <p class="lead">
-  <button type="button" class="btn btn-dark"><a href="https://docs.google.com/document/d/126gtEfAtpZ-Tas-d8hHcH9kegh5WEblML34rWzXxZ38/edit?usp=sharing" style={linkStyle}>View Full Resume</a></button>
+  <button type="button" class="btn btn-dark"><a href="https://docs.google.com/document/d/0B_KPR1M86f-gajJ1WGV5Y2ZYakpnZC1XZHJyVzFwNlMtNFNV/edit?resourcekey=0-DZ-GmcLnOYbopvSUt7rfDw" style={linkStyle}>View Full Resume</a></button>
   </p>
 </div>
     )
