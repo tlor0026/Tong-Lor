a20f438ab9fb5d0817066d080cce90ad919c4c6b
tlor0026 <tlor0026@gmail.com>
2022-10-14 17:44:06 -0500

    changes to packadges

--- src/components/about.js
+++ src/components/about.js
@@ -13,7 +13,7 @@ function About() {
     return(
         <div class="jumbotron">
   <h1 class="display-4">About Me</h1>
-  <img class="headshot" src= {photo} width="480px" height="800px" alt="headshot" style={imgStyle}></img>
+  <img class="headshot" src= {photo} width="420px" height="800px" alt="headshot" style={imgStyle}></img>
   <p class="lead">Welcome to my portfolio. I've been in the IT field for about 7 years now and decided to transition into software development/programing. Life is all about learning and being dedicated to building yourself up to new and better things. I've just completed my Coding boot camp with the University of Minnesota that went through a full stack course. This includes front and back end components, the user of server and 3rd party API's, React, Javascript, databases along with many other things. Im looking forward in life to more things to come!</p>
   <hr class="my-4"></hr>
   <p></p>
